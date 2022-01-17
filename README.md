fd187cf06417737847b12ba712e8f104cfb10dea
Dimas Sudjito <almuhtad@ualberta.ca>
2022-01-17 15:08:14 -0700

    setup apollo server, connect to db

---
+++ README.md
@@ -0,0 +1,5 @@
+# Logistic
+
+- Help logistic company keep tracks of valuables
+- Submission for Shopify backend internship challenge (Summer 2022)
+- Built with: TypeScript, Apollo Server, GraphQL, Mongoose, MongoDB
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,40 @@
-console.log('test')
+const { ApolloServer } = require('apollo-server')
+const mongoose = require('mongoose')
+
+require('dotenv').config()
+
+const PORT: string | number = process.env.PORT || 5000
+
+const gql = require('graphql-tag')
+const typeDefs = gql`
+  type Query {
+    sayHi: String!
+  }
+`
+
+const resolvers = {
+  Query: {
+    sayHi: () => 'Hello worlds'
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+mongoose
+  .connect(process.env.MONGODB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('MongoDB: logistic database connected')
+    return server.listen({ port: PORT })
+  })
+  .then((res: any) => {
+    console.log(`Server is running at ${res.url ? res.url : 'unknown url'}`)
+  })
+  .catch((err: any) => {
+    console.error(err)
+  })
