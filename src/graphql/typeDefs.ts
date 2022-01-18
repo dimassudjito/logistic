674e152ed7777a222d750a94de11a49013984508
Dimas Sudjito <almuhtad@ualberta.ca>
2022-01-17 18:40:17 -0700

    add typeDefs of location

--- src/graphql/typeDefs.ts
+++ src/graphql/typeDefs.ts
@@ -8,18 +8,29 @@ module.exports = gql`
     manufacturer: String
     location: String
   }
-  input ProductInput {
+  type Location {
+    id: ID!
     name: String!
+    city: String
+  }
+  input ProductInput {
+    name: String
     category: String
     manufacturer: String
     location: String
   }
+  input LocationInput {
+    name: String
+    city: String
+  }
   type Query {
     getProducts: [Product]
+    getLocations: [Location]
   }
   type Mutation {
     createProduct(productInput: ProductInput): Product!
     deleteProduct(productId: ID!): String!
     updateProduct(productId: ID!, productInput: ProductInput): Product!
+    createLocation(locationInput: LocationInput): Location
   }
 `
