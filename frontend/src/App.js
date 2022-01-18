291e1215fdb4d378fb80e685e3d9e06915c59e10
Dimas Sudjito <almuhtad@ualberta.ca>
2022-01-17 19:52:42 -0700

    display list of product

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,40 @@
 import React from 'react'
+import { useQuery, gql } from '@apollo/client'
+
+const GET_PRODUCTS = gql`
+  query GetProducts {
+    getProducts {
+      id
+      name
+      category
+      manufacturer
+    }
+  }
+`
 
 function App() {
-  return <div></div>
+  const { data } = useQuery(GET_PRODUCTS)
+  console.log(data.getProducts)
+  return (
+    <div>
+      <table>
+        <tr>
+          <th>Product</th>
+          <th>Category</th>
+          <th>Manufacturer</th>
+          <th>Location</th>
+        </tr>
+      </table>
+      {data.getProducts.map((product) => (
+        <tr>
+          <td>{product.name ? product.name : '<Empty>'}</td>
+          <td>{product.category ? product.category : '<Empty>'}</td>
+          <td>{product.manufacturer ? product.manufacturer : '<Empty>'}</td>
+          <td>{product.location ? product.location : '<Empty>'}</td>
+        </tr>
+      ))}
+    </div>
+  )
 }
 
 export default App
